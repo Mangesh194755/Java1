0abb236906e076c2d9ee0543fe3c75cb2c7d2470
Mangesh194755 <Mangesh194755@gmail.com>
2023-04-08 00:16:21 +0530

    Added new package JavaBasic1 and a class Strings1

---
+++ src/JavaBasics1/String1.java
@@ -0,0 +1,15 @@
+package JavaBasics1;
+
+public class String1 {
+	
+	public static void main(String[] args) {
+		
+		String company = "The Matright of know";
+		
+		System.out.println("Contains - "+company.contains("know"));
+		
+		System.out.println("Contains - "+company.charAt(14));
+		
+	}
+
+}
